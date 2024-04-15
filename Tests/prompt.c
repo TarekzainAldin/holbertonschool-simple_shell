2c31e46ccc917d551b37fe7917576c8dfbc03ffc
MrSandouiche <zemihimehdi1@gmail.com>
2024-04-15 14:07:11 +0000

    writing the prompt.c to follow the betty linter

--- Tests/prompt.c
+++ Tests/prompt.c
@@ -5,30 +5,44 @@
 
 int main(void)
 {
-    char *buffer = NULL;
-    size_t len = 0;
-    ssize_t nread;
+	char *buffer = NULL;
+	size_t len = 0;
+	ssize_t nread;
 
-    while (1)
-    {
-        printf("$ ");
-        nread = getline(&buffer, &len, stdin);
+	while (1)
+	{
+	printf("$ ");
+	nread = getline(&buffer, &len, stdin);
 
-        if (nread == -1) {
-            printf("Error reading input.\n");
-            break;
-        } else if (nread == 1) {
-            printf("End of file reached. Exiting...\n");
-            break;
-        } else if (strcmp(buffer, "EOD\n") == 0) {
-            // Vérifie si l'entrée est "EOD"
-            printf("Exiting...\n");
-            break;
-        } else {
-            printf("%s", buffer);
-        }
-    }
+	if (nread == -1)
+	{
+		printf("Error reading input.\n");
+	break;
+	}
+	else if (nread == 1)
+	{
+	printf("End of file reached. Exiting...\n");
+	break;
+	}
+	else if (strcmp(buffer, "EOD\n") == 0)
+	{
 
-    free(buffer); // Libération de la mémoire allouée par getline
-    return 0;
+	/* Vérifie si l'entrée t EOD
+	 *
+	 *
+	 */
+	printf("Exiting...\n");
+	break;
+	}
+	else
+	{
+	printf("%s", buffer);
+	}
+	}
+	free(buffer);
+
+	/*Libération de la mémoire allouée par g
+	 *
+	 */
+	return (0);
 }
